ca735f931d58d8acd474637c243c0165f7e85858
LiiChar <yura22875@yandex.ru>
2023-09-07 22:48:55 +0500

    initialize

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.weatherapi.com',
+            port: '',
+            // pathname: '/account123/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+
+
+export default () => {
+    const getItem = (key: string) => {
+        const store = localStorage.getItem(key);
+
+        if (!store) {
+            return '';
+        }
+
+        return JSON.parse(store);
+    }
+
+    const setItem = (key: string, data: any) => {
+        const JSONStore = JSON.stringify(data);
+        
+        localStorage.setItem(key, JSONStore);
+    }
+
+    const removeItem = (key: string) => {
+        localStorage.removeItem(key);
+    }
+    
+
+    return {
+        getItem,
+        setItem,
+        removeItem
+    }
+}
